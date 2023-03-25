c209a5d880f89634e8ea2bdadb66c640ee492f8a
Daniel Maciel Acevedo <anndannma@outlook.com>
2023-03-24 18:40:31 -0600

    Agregué un proyecto para obtener el Meta React Professional Certificate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  
+  const [score, setScore] = useState("");
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Number(score) <= 3 && comment.length <= 10) {
+      alert("Please, provide a comment with more info about your poor experience!")
+    } else {
+      console.log("Form submited!")
+      setScore("✅")
+      setComment("")
+    }
+  }
+
+  function starsScore() {
+    if (Number(score) == 1) {
+      return "⭐"
+    } else if (score == 2) {
+      return "⭐ ⭐"
+    } else if (score == 3) {
+      return "⭐ ⭐ ⭐"
+    } else if (score == 4) {
+      return "⭐ ⭐ ⭐ ⭐"
+    } else if (score == 5) {
+      return "⭐ ⭐ ⭐ ⭐ ⭐"
+    } else if (score == "✅") {
+      return "✅"
+    } else {
+      return "👇"
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='AppContent'>
+        <form onSubmit={handleSubmit}>
+          <fieldset className='Fieldset'>
+            <div className='Field'>
+              <label>Rate our service</label>
+              <h1>{starsScore()}</h1>
+              <div className='RangeField'>
+                <p>Worst</p>
+                <input type="range" min="1" max="5" value={score} onChange={e => setScore(e.target.value)} />
+                <p>Excelent</p>
+              </div>
+            </div>
+            <div className='Field'>
+              <label>Comment</label>
+              <textarea id="CommentArea" type="text" value={comment} onChange={e => setComment(e.target.value)} />
+            </div>
+            <button className="SubmitButton" type="submit" disabled={!score} >Submit</button>
+          </fieldset>
+        </form>
+      </div>
+
+      <div className="PoweredByDanielMacielAcevedo">
+        <p>Powered by Daniel Maciel Acevedo</p>
+        <a href="https://github.com/danielmacielacevedo/meta-react-feedback-form" ><span></span>ver codigo</a>
+      </div>
     </div>
   );
 }
